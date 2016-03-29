1d225f95755203ec639c71b47e7c340f1891d9fa
Remigijus Vainoris <remi@Remigijuss-MacBook-Pro.local>
2016-03-29 10:56:44 +0100

    MESSAGE

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,7 +4,7 @@ var ctrlMain = require('../controllers/pages');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
-router.get('quote/random', ctrlMain.random);
+router.get('/quote/random', ctrlMain.random);
 router.post('/submitQuote', ctrlMain.submitQuote);
 
 module.exports = router;
